6bf3b5f0c0fec1f9fdd3aa4ae263f17e2919932a
Himanshu Dixit <hudixt@gmail.com>
2022-09-08 10:03:07 +0000

    .

--- action.mjs
+++ action.mjs
@@ -1,4 +1,15 @@
 #!/usr/bin/env zx
+const {PRETTIER_DIR, PUTOUT_DIR,GITHUB_WORKSPACE}= process.env
 
-let name = 'foo & bar'
-console.log(process.env)
+console.log({PRETTIER_DIR, PUTOUT_DIR})
+
+for(let dir of PRETTIER_DIR.split(" ")){
+    const dirPath = `${GITHUB_WORKSPACE}/${dir}`;
+    await `cd ${dirPath} && npx prettier --write . `
+}
+
+for(let dir of PUTOUT_DIR.split(" ")){
+    const dirPath = `${GITHUB_WORKSPACE}/${dir}`;
+    await `cd ${dirPath} && npx putout --fix . `
+}
+
