8a737bac05b2e9fbefa4f3da4aaba5c750f04114
Himanshu Dixit <hudixt@gmail.com>
2022-09-08 11:14:01 +0000

    .

--- action.mjs
+++ action.mjs
@@ -1,16 +1,17 @@
 #!/usr/bin/env zx
 const {PRETTIER_DIR, PUTOUT_DIR,GITHUB_WORKSPACE}= process.env
 
-console.log({PRETTIER_DIR, PUTOUT_DIR})
+const data = await $`ls -la`;
+
 
 for(let dir of PRETTIER_DIR.split(" ")){
     const dirPath = `${GITHUB_WORKSPACE}/${dir}`;
     console.log(`cd ${dirPath} && npx prettier --write . `)
-    await `cd ${dirPath} && npx prettier --write . `
+    await $`cd ${dirPath} && npx prettier --write . `
 }
 
 for(let dir of PUTOUT_DIR.split(" ")){
     const dirPath = `${GITHUB_WORKSPACE}/${dir}`;
-    await `cd ${dirPath} && npx putout --fix . `
+    await $`cd ${dirPath} && npx putout --fix . `
 }
 
